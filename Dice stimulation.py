32ab615273bce85b1cd4dde9218a5e78faf84d37
Daksh Meshram <78013164+Daksh-ai@users.noreply.github.com>
2021-06-25 11:26:27 +0530

    Add files via upload

---
+++ Dice stimulation.py
@@ -0,0 +1,53 @@
+import random
+while(True):
+    x=random.randint(1,6)
+    if x==1:
+        print("_______")
+        print("|     |")
+        print("|     |")
+        print("|  *  |")
+        print("|     |")
+        print("|_____|")
+        break
+    elif x==2:
+        print("_______")
+        print("|     |")
+        print("|  *  |")
+        print("|     |")
+        print("|  *  |")
+        print("|_____|")
+        break
+    elif x==3:
+        print("_______")
+        print("|     |")
+        print("|  *  |")
+        print("|  *  |")
+        print("|  *  |")
+        print("|_____|")
+        break
+    elif x==4:
+        print("_______")
+        print("|     |")
+        print("| * * |")
+        print("|     |")
+        print("| * * |")
+        print("|_____|")
+        break
+    elif x==5:
+        print("_______")
+        print("|     |")
+        print("| * * |")
+        print("|  *  |")
+        print("| * * |")
+        print("|_____|")
+        break
+        
+    elif x==6:
+        print("_______")
+        print("|     |")
+        print("| * * |")
+        print("| * * |")
+        print("| * * |")
+        print("|_____|")
+        break
+    
